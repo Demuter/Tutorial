ebba4ab555976b554852faaa858b1e70be238ea8
Demuter <malashenkov02.09.93@gmail.com>
2018-01-25 10:45:23 +0300

    Added class TernaryQuadrant. Which shows in which quadrant the point is located by coordinates.

---
+++ src/conditions/TernaryQuadrant.java
@@ -0,0 +1,28 @@
+package conditions;
+
+public class TernaryQuadrant {
+    public static void main(String[] args) {
+
+        int x = 10, y = 5;
+
+        String quadrant0, quadrant1,quadrant2;
+
+        //                   condition    true block    false block     condition   true block       false block
+        //                    -------- ? ------------ : -------------    -------- ? ------------- : --------------
+        quadrant0 = (x > 0) ? ((y > 0) ? "I quadrant" : "IV quadrant") : ((y > 0) ? "II quadrant" : "III quadrant");
+        //          ------- ? ---------------------------------------- : -------------------------------------------
+        //         condition               true block                                   false block
+
+        // ... or
+
+        quadrant1 = x > 0 ? (y > 0 ? "I quadrant" : "IV quadrant") : (y > 0 ? "II quadrant" : "III quadrant");
+
+        // ... or
+
+        quadrant2 = x > 0 ? y > 0 ? "I quadrant" : "IV quadrant" : y > 0 ? "II quadrant" : "III quadrant";
+
+        System.out.println(quadrant0);
+        System.out.println(quadrant1);
+        System.out.println(quadrant2);
+    }
+}
