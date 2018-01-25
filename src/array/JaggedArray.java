94cbd32955ae787408f8a8ab869d36e5a5d12a66
Demuter <malashenkov02.09.93@gmail.com>
2018-01-25 12:48:43 +0300

    Added class JaggedArray.

---
+++ src/array/JaggedArray.java
@@ -0,0 +1,20 @@
+package array;
+
+public class JaggedArray {
+
+    public static void main(String[] args) {
+        int[][] jagged = new int[][]
+                {
+                        new int[]{1, 2},
+                        new int[]{1, 2, 3, 4, 5},
+                        new int[]{1, 2, 3}
+                };
+
+        for (int i = 0; i < jagged.length; ++i) {
+            for (int j = 0; j < jagged[i].length; ++j) {
+                System.out.print(" " + jagged[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
+}
