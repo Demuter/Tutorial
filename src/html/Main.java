bf31eaa604378894a412ea69c7ac61a7a23302b2
Demuter <malashenkov02.09.93@gmail.com>
2018-02-20 13:12:28 +0300

    Added project with jvisualvm.

--- src/html/Main.java
+++ src/html/Main.java
@@ -3,7 +3,8 @@ package html;
 public class Main {
     public static void main(String[] args) {
         ParseHtmlByJsoup parse = new ParseHtmlByJsoup();
-        String teamsName[] = new String[11];
+        String teamsName[] = new String[21];
+
         teamsName[0] = "la-liga-table";
         teamsName[1] = "eredivisie-table";
         teamsName[2] = "serie-a-table";
@@ -15,6 +16,16 @@ public class Main {
         teamsName[8] = "scottish-premier-table";
         teamsName[9] = "league-1-table";
         teamsName[10] = "league-2-table";
+        teamsName[11] = "world-cup-table";
+        teamsName[12] = "chinese-super-league-table";
+        teamsName[13] = "womens-premier-league-northern-division-table";
+        teamsName[14] = "womens-premier-league-southern-division-table";
+        teamsName[15] = "football/competitions/scottish-championship/table";
+        teamsName[16] = "football/competitions/scottish-league-one/table";
+        teamsName[17] = "football/competitions/scottish-league-two/table";
+        teamsName[18] = "football/competitions/vanarama-national-league/table";
+        teamsName[19] = "football/competitions/vanarama-national-league-north/table";
+        teamsName[20] = "football/competitions/vanarama-national-league-south/table";
 
         for (int i = 0; i < teamsName.length; i++) {
             parse.parserHTML(teamsName[i]);
--- src/html/ParseHtmlByJsoup.java
+++ src/html/ParseHtmlByJsoup.java
@@ -8,47 +8,28 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 
 public class ParseHtmlByJsoup {
+
+
 
     public void parserHTML(String s) {
 
         List<String> parseListHtml = new ArrayList<>();
-        String tmp;
         try {
-            File statText = new File("G:/Tutorial/src/html/statsTest.txt");
-            FileOutputStream is = new FileOutputStream(statText);
-            OutputStreamWriter osw = new OutputStreamWriter(is);
-            Writer w = new BufferedWriter(osw);
-
             Document document = Jsoup.connect("http://www.skysports.com/" + s).get();
             Elements clubRow = document.select("tr.standing-table__row");
             for (Element club : clubRow) {
                 parseListHtml.add(club.select("td.standing-table__cell").text());
-                tmp = parseListHtml.toString();
-                w.write(tmp);
-                w.close();
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-
-        for (int i = 1; i < parseListHtml.size(); i++) {
-           // System.out.println(parseListHtml.get(i));
-
-            
-        }
-
-
+//      for (int i = 1; i < parseListHtml.size(); i++) {
+//          System.out.println(parseListHtml.get(i));
+//      }
     }
 }
 
